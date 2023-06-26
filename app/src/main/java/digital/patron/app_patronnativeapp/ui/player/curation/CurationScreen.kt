cdc1d8dc5b6dfb907d002ccee84cf42dcc969448
constant94-dev <constant94@naver.com>
2023-06-26 17:38:58 +0900

    [0626] init 블록 주석 & 플레이어 UI 상단고정

--- app/src/main/java/digital/patron/app_patronnativeapp/ui/player/curation/CurationScreen.kt
+++ app/src/main/java/digital/patron/app_patronnativeapp/ui/player/curation/CurationScreen.kt
@@ -119,9 +119,9 @@ fun CurationView() {
         }, // topBar end
         containerColor = Color(R.color.background),
     ) {
+
         Column(
             modifier = Modifier
-                .verticalScroll(vtScrollState)
                 .fillMaxSize(),
         ) {
 
@@ -155,178 +155,188 @@ fun CurationView() {
                 }
             } // 플레이어 UI 끝
 
-            Text(
-                text = "&큐레이션 타이틀",
+            Column(
                 modifier = Modifier
-                    .padding(top = 8.dp, start = 16.dp, end = 16.dp),
-                color = Title,
-                fontWeight = FontWeight(700),
-                fontSize = 20.sp,
-            )
-
-            Text(
-                text = "&작가이름",
-                modifier = Modifier
-                    .padding(top = 4.dp, start = 16.dp, end = 16.dp),
-                color = SubTitle,
-                fontWeight = FontWeight(400),
-                fontSize = 16.sp,
-            )
-
-            Text(
-                text = "&작품수 · &작품감상시간",
-                modifier = Modifier
-                    .padding(top = 4.dp, start = 16.dp, end = 16.dp),
-                color = SubTitle,
-                fontWeight = FontWeight(400),
-                fontSize = 12.sp,
-            )
-
-            Text(
-                text = "&작품인트로",
-                modifier = Modifier
-                    .padding(top = 4.dp, start = 16.dp, end = 16.dp),
-                color = SubTitle,
-                fontWeight = FontWeight(400),
-                fontSize = 12.sp,
-            )
-
-            Row(
-                modifier = Modifier
-                    .padding(top = 16.dp, bottom = 16.dp, start = 16.dp, end = 16.dp)
-                    .fillMaxWidth(),
+                    .verticalScroll(vtScrollState)
+                    .fillMaxSize(),
             ) {
-                IconButton(
-                    onClick = {},
-                ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.icon_save_collection),
-                        contentDescription = "save collection.",
-                        colorFilter = ColorFilter.tint(Color.White),
-                    )
-                }
-                IconButton(
-                    onClick = {},
-                ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.icon_like),
-                        contentDescription = "user like.",
-                        colorFilter = ColorFilter.tint(Color.White),
-                    )
-                }
-                IconButton(
-                    onClick = {},
-                ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.icon_share),
-                        contentDescription = "user share.",
-                        colorFilter = ColorFilter.tint(Color.White),
-                    )
-                }
-                IconButton(
-                    onClick = {},
-                ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.icon_show_option_white),
-                        contentDescription = "show option.",
-                        colorFilter = ColorFilter.tint(Color.White),
-                    )
-                }
-            } // 아이콘 UI 4개
 
-            for (i in 1..15) {
+                Text(
+                    text = "&큐레이션 타이틀",
+                    modifier = Modifier
+                        .padding(top = 8.dp, start = 16.dp, end = 16.dp),
+                    color = Title,
+                    fontWeight = FontWeight(700),
+                    fontSize = 20.sp,
+                )
+
+                Text(
+                    text = "&작가이름",
+                    modifier = Modifier
+                        .padding(top = 4.dp, start = 16.dp, end = 16.dp),
+                    color = SubTitle,
+                    fontWeight = FontWeight(400),
+                    fontSize = 16.sp,
+                )
+
+                Text(
+                    text = "&작품수 · &작품감상시간",
+                    modifier = Modifier
+                        .padding(top = 4.dp, start = 16.dp, end = 16.dp),
+                    color = SubTitle,
+                    fontWeight = FontWeight(400),
+                    fontSize = 12.sp,
+                )
+
+                Text(
+                    text = "&작품인트로",
+                    modifier = Modifier
+                        .padding(top = 4.dp, start = 16.dp, end = 16.dp),
+                    color = SubTitle,
+                    fontWeight = FontWeight(400),
+                    fontSize = 12.sp,
+                )
+
                 Row(
                     modifier = Modifier
-                        .padding(start = 16.dp, end = 16.dp, bottom = 8.dp)
-                        .height(45.dp)
+                        .padding(top = 16.dp, bottom = 16.dp, start = 16.dp, end = 16.dp)
                         .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Start,
                 ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.img_empty),
-                        contentDescription = "player list.",
-                        contentScale = ContentScale.Crop,
-                        modifier = Modifier
-                            .size(45.dp, 45.dp)
-                            .clip(RoundedCornerShape(4.dp)),
-                    )
-                    Column(
-                        modifier = Modifier
-                            .align(CenterVertically)
-                            .weight(0.7f)
-                            .padding(start = 8.dp),
+                    IconButton(
+                        onClick = {},
                     ) {
-                        Text(
-                            text = "&아트워크 타이틀",
-                            color = Title,
-                            fontWeight = FontWeight(500),
-                            fontSize = 16.sp,
-                        )
-                        Text(
-                            text = "&아티스트 타이틀",
-                            color = SubTitle,
-                            fontWeight = FontWeight(400),
-                            fontSize = 14.sp,
+                        Image(
+                            painter = painterResource(id = R.drawable.icon_save_collection),
+                            contentDescription = "save collection.",
+                            colorFilter = ColorFilter.tint(Color.White),
                         )
                     }
-                    Image(
-                        painter = painterResource(id = R.drawable.icon_show_option_white),
-                        contentDescription = "player list.",
+                    IconButton(
+                        onClick = {},
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.icon_like),
+                            contentDescription = "user like.",
+                            colorFilter = ColorFilter.tint(Color.White),
+                        )
+                    }
+                    IconButton(
+                        onClick = {},
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.icon_share),
+                            contentDescription = "user share.",
+                            colorFilter = ColorFilter.tint(Color.White),
+                        )
+                    }
+                    IconButton(
+                        onClick = {},
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.icon_show_option_white),
+                            contentDescription = "show option.",
+                            colorFilter = ColorFilter.tint(Color.White),
+                        )
+                    }
+                } // 아이콘 UI 4개
+
+                for (i in 1..15) {
+                    Row(
                         modifier = Modifier
-                            .align(CenterVertically)
-                            .weight(0.1f),
-                        colorFilter = ColorFilter.tint(Color.White),
-                    )
-                }
-            } // 아트워크 리스트 UI 끝
-
-            Spacer(modifier = Modifier.height(32.dp))
-
-
-            Text(
-                text = "참여 아티스트",
-                modifier = Modifier.padding(
-                    bottom = 8.dp, start = 16.dp, end = 16.dp
-                ),
-                color = Title,
-                fontWeight = FontWeight(500),
-                fontSize = 18.sp,
-            )
-            LazyRow(
-                modifier = Modifier
-                    .padding(start = 16.dp, end = 16.dp),
-                horizontalArrangement = Arrangement.spacedBy(12.dp),
-            ) {
-                items(15) {
-                    Column {
+                            .padding(start = 16.dp, end = 16.dp, bottom = 8.dp)
+                            .height(45.dp)
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Start,
+                    ) {
                         Image(
                             painter = painterResource(id = R.drawable.img_empty),
-                            contentDescription = "",
+                            contentDescription = "player list.",
                             contentScale = ContentScale.Crop,
                             modifier = Modifier
-                                .size(154.dp)
-                                .clip(CircleShape),
+                                .size(45.dp, 45.dp)
+                                .clip(RoundedCornerShape(4.dp)),
                         )
-                        Text(
-                            text = "&아티스트 이름",
+                        Column(
                             modifier = Modifier
-                                .width(154.dp)
-                                .padding(top = 4.dp),
-                            color = Title,
-                            fontWeight = FontWeight(500),
-                            fontSize = 16.sp,
-                            textAlign = TextAlign.Center,
+                                .align(CenterVertically)
+                                .weight(0.7f)
+                                .padding(start = 8.dp),
+                        ) {
+                            Text(
+                                text = "&아트워크 타이틀",
+                                color = Title,
+                                fontWeight = FontWeight(500),
+                                fontSize = 16.sp,
+                            )
+                            Text(
+                                text = "&아티스트 타이틀",
+                                color = SubTitle,
+                                fontWeight = FontWeight(400),
+                                fontSize = 14.sp,
+                            )
+                        }
+                        Image(
+                            painter = painterResource(id = R.drawable.icon_show_option_white),
+                            contentDescription = "player list.",
+                            modifier = Modifier
+                                .align(CenterVertically)
+                                .weight(0.1f),
+                            colorFilter = ColorFilter.tint(Color.White),
                         )
                     }
+                } // 아트워크 리스트 UI 끝
+
+                Spacer(modifier = Modifier.height(32.dp))
+
+
+                Text(
+                    text = "참여 아티스트",
+                    modifier = Modifier.padding(
+                        bottom = 8.dp, start = 16.dp, end = 16.dp
+                    ),
+                    color = Title,
+                    fontWeight = FontWeight(500),
+                    fontSize = 18.sp,
+                )
+                LazyRow(
+                    modifier = Modifier
+                        .padding(start = 16.dp, end = 16.dp),
+                    horizontalArrangement = Arrangement.spacedBy(12.dp),
+                ) {
+                    items(15) {
+                        Column {
+                            Image(
+                                painter = painterResource(id = R.drawable.img_empty),
+                                contentDescription = "",
+                                contentScale = ContentScale.Crop,
+                                modifier = Modifier
+                                    .size(154.dp)
+                                    .clip(CircleShape),
+                            )
+                            Text(
+                                text = "&아티스트 이름",
+                                modifier = Modifier
+                                    .width(154.dp)
+                                    .padding(top = 4.dp),
+                                color = Title,
+                                fontWeight = FontWeight(500),
+                                fontSize = 16.sp,
+                                textAlign = TextAlign.Center,
+                            )
+                        }
+                    }
                 }
-            }
 
-            Spacer(
-                modifier = Modifier
-                    .height(80.dp)
-            )
+                Spacer(
+                    modifier = Modifier
+                        .height(80.dp)
+                )
 
-        } // 콘텐츠 전체 UI 끝
+            } // 콘텐츠 전체 UI 끝
+        }
+
+
+
     } // Scaffold UI 끝
 }
 
